d3f8a1037c72819f9999547f75fa3d2744f0b595
Aleksejs Muzicenko <a.muzicenko@multipass.co>
2024-09-10 12:42:41 +0300

    chore: init

---
+++ build/rollup.config.base.js
@@ -0,0 +1,31 @@
+import babel from 'rollup-plugin-babel';
+import vue from 'rollup-plugin-vue';
+import cjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import postcss from 'rollup-plugin-postcss';
+import analyze from 'rollup-plugin-analyzer';
+
+const config = require('../package.json');
+
+export default {
+  input: 'src/index.js',
+  plugins: [
+    vue({
+      css: true,
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      runtimeHelpers: true,
+      extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.vue'],
+    }),
+    cjs(),
+    replace({
+      VERSION: JSON.stringify(config.version),
+    }),
+    postcss(),
+    analyze(),
+  ],
+  watch: {
+    include: 'src/**',
+  },
+};
---
+++ build/rollup.config.umd.js
@@ -0,0 +1,19 @@
+import resolve from 'rollup-plugin-node-resolve';
+import base from './rollup.config.base';
+
+const config = Object.assign({}, base, {
+  output: {
+    exports: 'named',
+    name: 'vue-tel-input',
+    file: 'dist/vue-tel-input.umd.js',
+    format: 'umd',
+  },
+});
+
+config.plugins.push(
+  resolve({
+    mainFields: ["module", "jsnext:main", "main", "browser"]
+  })
+);
+
+export default config;
